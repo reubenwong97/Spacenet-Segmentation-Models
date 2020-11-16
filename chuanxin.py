95b7149a28622e9a2d824115d653c6a3d440b2af
tanchuanxin <chuanxin2008@hotmail.com>
2020-11-16 15:37:22 +0800

    Update chuanxin.py

--- chuanxin.py
+++ chuanxin.py
@@ -9,7 +9,7 @@ SM_FRAMEWORK = os.getenv('SM_FRAMEWORK')
 
 import segmentation_models as sm
 sm.set_framework(SM_FRAMEWORK)
-BACKBONE = 'resnet34'
+BACKBONE = 'resnet50'
 preprocess_input = sm.get_preprocessing(BACKBONE)
 
 import wandb
@@ -24,11 +24,13 @@ print("X_train, Y_train, X_test, Y_test loaded")
 
 # preprocess input
 print("preprocessing input")
-Y_train = tf.dtypes.cast(Y_train, tf.float32)
-Y_test = tf.dtypes.cast(Y_test, tf.float32)
 X_train = preprocess_input(X_train)
 X_test = preprocess_input(X_test)
+Y_train = tf.dtypes.cast(Y_train, tf.float32)
+Y_test = tf.dtypes.cast(Y_test, tf.float32)
 print("finished preprocessing input")
+
+print(X_train.shape)
 
 # input subset of data only
 # X_train, Y_train, X_test, Y_test = X_train[:100], Y_train[:100], X_test[:100], Y_test[:100]
@@ -36,18 +38,18 @@ print("finished preprocessing input")
 # define model
 model = sm.Unet(BACKBONE, encoder_weights='imagenet', input_shape=(None, None, 3))
 model.compile(
-    optimizer='Adam',
-    loss=sm.losses.bce_jaccard_loss,
-    metrics=[sm.metrics.iou_score],
+    optimizer='adam',
+    loss=sm.losses.BinaryFocalLoss(alpha=0.75, gamma=0.25),
+    metrics=[sm.metrics.IOUScore],
 )
 
 # fit model
 # if you use data generator use model.fit_generator(...) instead of model.fit(...)
 # more about `fit_generator` here: https://keras.io/models/sequential/#fit_generator
-model.fit(
-   x=X_train,
-   y=Y_train,
-   batch_size=32,
-   epochs=100,
-   validation_split=0.2
-)
+# model.fit(
+#    x=X_train,
+#    y=Y_train,
+#    batch_size=32,
+#    epochs=10000,
+#    validation_split=0.2
+# )
