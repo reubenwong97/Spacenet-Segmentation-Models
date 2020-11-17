9bd0ce05f2a808e453f89b158e4857d66765e9bc
#WONG YUH SHENG REUBEN# <wong1109@SCSEGPU-TC1.cm.cluster>
2020-11-17 13:59:49 +0800

    resnet50 run

---
+++ job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=spacenet_6
+#SBATCH --output=spacenet_6.out
+#SBATCH --error=spacenet_6.err
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --mem=8000M
+#SBATCH --time=04:00:00
+#SBATCH --ntasks-per-node=8
+#SBATCH --qos=normal
+#SBATCH --partition=SCSEGPU_UG
+
+module load anaconda
+conda activate tf2.1
+python $1
+
