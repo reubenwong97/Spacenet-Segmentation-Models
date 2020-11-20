918137cb864ba23af9e002b0cea7896ab78b5156
tanchuanxin <chuanxin2008@hotmail.com>
2020-11-20 11:30:38 +0800

    update scripts for inspecting results

--- results/histories/histories_score.py
+++ results/histories/histories_score.py
@@ -4,6 +4,8 @@ import os
 fnames = next(os.walk('.'))[2]
 fnames = [x for x in fnames if x[-4:] == '.npy']
 
+best_model = ['', 0, 0]
+
 for fname in fnames:
     if "learningrate" in fname:
         history = np.load(fname, allow_pickle=True)
@@ -14,3 +16,10 @@ for fname in fnames:
         max_val_iou_score = history['val_iou_score'][index_min]
 
         print(f'{fname}: \nMin val_loss: {min_val_loss}, Max val_iou_score:{max_val_iou_score}\n')
+
+        if max_val_iou_score > best_model[2]:
+            best_model[0] = fname
+            best_model[1] = min_val_loss
+            best_model[2] = max_val_iou_score
+
+print(f"best model: {best_model}")
--- results/predictions/prediction_scores.py
+++ results/predictions/prediction_scores.py
@@ -4,7 +4,17 @@ import os
 fnames = next(os.walk('.'))[2]
 fnames = [x for x in fnames if x[-4:] == '.npy']
 
+best_model = ['', 0, 0]
+
 for fname in fnames:
     if "learningrate" in fname:
         score = np.load(fname, allow_pickle=True)
+        score = score.item()
         print(f'{fname}: \n{score}\n')
+
+        if score['test_iou_score'] > best_model[2]:
+            best_model[0] = fname
+            best_model[1] = score['test_loss']
+            best_model[2] = score['test_iou_score']
+
+print(f"best model: {best_model}")
