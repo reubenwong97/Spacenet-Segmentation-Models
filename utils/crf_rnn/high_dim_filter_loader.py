fe47f6207e04cf2f7f509887dc4976b9c3c5adb1
tanchuanxin <chuanxin2008@hotmail.com>
2020-11-21 22:59:20 +0800

    reruns and CRF

--- utils/crf_rnn/high_dim_filter_loader.py
+++ utils/crf_rnn/high_dim_filter_loader.py
@@ -26,12 +26,12 @@ print('__file__ is', __file__)
 
 import os
 import tensorflow as tf
-from tensorflow.python.framework import ops
+# from tensorflow.python.framework import ops
 custom_module = tf.load_op_library(os.path.join(os.path.dirname(__file__), 'cpp', 'high_dim_filter.so'))
 
 print(__file__)
 
-@ops.RegisterGradient('HighDimFilter')
+@tf.RegisterGradient('HighDimFilter')
 def _high_dim_filter_grad(op, grad):
     """ Gradients for the HighDimFilter op. We only need to calculate the gradients
     w.r.t. the first input (unaries) as we never need to backprop errors to the
