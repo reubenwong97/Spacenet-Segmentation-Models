f86652848ef1af04f6ab683b116c9191b9ecc00a
Reuben Wong <reuben.wong.ys@gmail.com>
2020-11-21 15:40:45 +0800

    remove build dirs

--- build_dirs.py
+++
@@ -1,8 +0,0 @@
-import os
-
-if not os.isdir('./data_project'):
-    os.makedirs('./data_project')
-    if not os.isdir('./data_project/train'):
-        os.makedirs('./data_project/train')
-    if not os.isdir('./data_project/test'):
-        os.makedirs('./data_project/test')
